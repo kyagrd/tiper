02e05da58a6f2c826f515b547b6ec5bd1a5ca841
Ki Yung Ahn <kyagrd@gmail.com>
2015-12-06 17:26:44 +0900

    a comment fix

--- mapunify.pl
+++ mapunify.pl
@@ -14,7 +14,7 @@ split_heads([],[]-[]).
 split_heads([X:V|T],[X:V]-T) :- var(T), !, true.
 split_heads([X:V|Ps],[X:V|Hs]-T) :- split_heads(Ps,Hs-T).
 
-% helper function for unify_oeset (almost no duplicate answers)
+% helper function for unify_oemap
 unify_oe_map(Xs-T1,Ys-T2) :- T1==[], T2==[], unify_map(Xs,Ys).
 unify_oe_map(Xs-T1,Ys-T2) :- T1==[], submap_of(Ys,Xs), mapminus(Xs,Ys,T2).
 unify_oe_map(Xs-T1,Ys-T2) :- T2==[], submap_of(Xs,Ys), mapminus(Ys,Xs,T1).
@@ -22,4 +22,7 @@ unify_oe_map(Xs-T1,Ys-T2) :-
 	mapminus(Ys,Xs,L1), append(L1,T,T1),
        	mapminus(Xs,Ys,L2), append(L2,T,T2).
 
-
+%% ?- unify_oemap([z:string,y:bool|M1],[y:T,x:int|M2]).
+%% M1 = [x:int|_G1426],
+%% T = bool,
+%% M2 = [z:string|_G1426].
